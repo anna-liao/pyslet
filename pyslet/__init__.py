decda9fe03dc7fa42e49845b00f414aa3b555737
Anna Liao <aliao22@gmail.com>
2016-12-27 09:38:23 -0700

    updated gift unit test

--- pyslet/__init__.py
+++
@@ -1,5 +0,0 @@
-"""A package for standards for learning, education and training"""
-
-# Define the list of modules to import when clients use:
-# from pyslet import *
-__all__ = []
--- pyslet/gift/gifttxt.py
+++ pyslet/gift/gifttxt.py
@@ -1,11 +1,30 @@
 
 
 
-class GIFTdocument:
+class GIFTDocument(giftns.GIFTNSDocument):
 	"""
 	Same structure as qtixml.QTIDocument
+	https://github.com/swl10/pyslet/blob/master/pyslet/qtiv2/xml.py#L23
+
+	Used to represent all documents representing information from the
+	GIFT specification (https://docs.moodle.org/23/en/GIFT_format).
+
+	Simple recipe to get started::
+
+		import pyslet.gift.gifttxt as gifttxt
+
+		doc = gifttxt.GIFTDocument()
+		with open('mygift.txt', 'rb') as f:
+			doc.read(src=f)
+			# do stuff with the GIFT document here
+
+	The root (doc.root) element of a GIFT document may be one of a number
+	of elements.  Look to implement an element like qtiv2.items.AssessmentItem
+	https://github.com/swl10/pyslet/blob/master/pyslet/qtiv2/items.py#L15
 
 	"""
+
+	classMap = {}
 
 	def __init__(self):
 		pass
