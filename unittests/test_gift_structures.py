ce8960d21e0083c3c1ec18ab940031614d94fb5e
Anna Liao <aliao22@gmail.com>
2017-01-04 10:58:18 -0700

    added GIFTCharacterTests.test_space to test_gift_structures

--- unittests/test_gift_structures.py
+++ unittests/test_gift_structures.py
@@ -7,10 +7,20 @@ from pyslet.gift import structures
 
 def suite():
 	return unittest.TestSuite((
+		unittest.makeSuite(GIFTCharacterTests, 'test'),
 		unittest.makeSuite(GIFTEntityTests, 'test')
 		# unittest.makeSuite(ElementTests, 'test'),
 		# unittest.makeSuite(DocumentTests, 'test')
 	))
+
+
+class GIFTCharacterTests(unittest.TestCase):
+
+	def test_space(self):
+		"""[3] S ::= (#x20 | #x9 | #xD | #xA)+"""
+		expected_edges = [0x9, 0xB, 0xD, 0xE, 0x20, 0x21]
+		self.assertTrue(self.find_edges(structures.is_s, 256) ==
+			expected_edges, "is_s")
 
 
 class GIFTEntityTests(unittest.TestCase):
