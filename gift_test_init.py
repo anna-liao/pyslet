56ffb8556f03bcf5484f2f9659f088f2d97ed54a
Anna Liao <aliao22@gmail.com>
2017-02-14 14:08:42 -0700

    add gift test scripts

---
+++ gift_test_init.py
@@ -0,0 +1,17 @@
+from unittests.test_gift_parser import *
+
+# s = """::Q1::1+1=2{T}"""
+# s = """//comment::Question title::Question{=A correct answer ~Wrong answer1}"""
+s = """//comment\n::Question title\n::Question{\n=A correct answer\n~Wrong answer1\n}"""
+e = structures.GIFTEntity(s)
+p = parser.GIFTParser(e)
+element = p.element = structures.Element("a")
+p.parse_element()
+children = list(element.get_children())
+
+print("Length of children: {}".format(len(children)))
+for child in children:
+	if isinstance(child, structures.Element):
+		print("{}, {}".format(child.giftname, child.get_value()))
+	else:
+		print(child)
